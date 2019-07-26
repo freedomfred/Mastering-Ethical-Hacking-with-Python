a5d5a0883bf5ed7ae431cce2cde7fcf30f3f5968
freedomfred <freedomfred@gmail.com>
2019-07-25 18:56:00 -0700

    working listener

--- Ch3/DNSServer.py
+++ Ch3/DNSServer.py
@@ -1,10 +1,12 @@
+
 import threading
-import SocketServer
+import socket
+import socketserver
 import dnslib
 
 
 
-class TCPDNSHandler(SocketServer.BaseRequestHandler):
+class TCPDNSHandler(socketserver.BaseRequestHandler):
 
 
     def handle(self):
@@ -12,11 +14,6 @@ class TCPDNSHandler(SocketServer.BaseRequestHandler):
         self.data = self.request.recv(8192).strip()
         print("{} wrote:".format(self.client_address[0]))
         
-        sz = int(self.data[:2].encode('hex'), 16)
-        if sz < len(self.data) - 2:
-            raise Exception("Wrong size of TCP packet")
-        elif sz > len(self.data) - 2:
-            raise Exception("Too big TCP packet")
 
         req = dnslib.DNSRecord.parse(self.data[2:])
         print(req)
@@ -25,11 +22,12 @@ class TCPDNSHandler(SocketServer.BaseRequestHandler):
         self.request.sendall(self.data.upper())
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 53
+    HOST, PORT = socket.gethostname(), 53
 
     # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), TCPDNSHandler)
+    server = socketserver.TCPServer((HOST, PORT), TCPDNSHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
